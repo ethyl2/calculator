f0f02ca3b423b2e72c7dfb057a8851bb9af8c911
Heather Nuffer <zzzuzz@gmail.com>
2020-09-03 13:52:49 -0600

    Initial commit with layout setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [topDisplay, setTopDisplay] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <div className="calculator-box">
+        <div className="top-display">{topDisplay}</div>
+        <div className="buttons-area">
+          <div className="left-side">
+            <div className="clear-button">C</div>
+            <div className="digit-buttons">
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+            <div className="zero-button">0</div>
+          </div>
+
+          <div className="right-side">
+            <div>÷</div>
+            <div>x</div>
+            <div>-</div>
+            <div>+</div>
+            <div className="equal-button">=</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
